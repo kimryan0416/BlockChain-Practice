380d219c046b7cd3e7b68dadd2a31f3fcd7ffba7
rk494 <rk494@cornell.edu>
2019-05-29 14:35:21 +0900

    Update #1: Added Proof of Work concept into blockchain to allow for mining blocks and improving the functionality of adding valid blocks

--- app.js
+++ app.js
@@ -7,16 +7,34 @@ class Block {
 		this.data = data;
 		this.previousHash = previousHash;
 		this.hash = this.calculateHash();
+		// When we mine a block, the data above will always produce the same hash value. Therfore, when we initiate
+		// the While loop inside "mineBlock", the loop will go on endlessly.
+		// In order to prevent this, we add a value "nonce" that doesn't really DO anything or MEAN anything, except
+		// that it will change the hash value regardless. think of it like the telomeres of a DNA or something, conceptually.
+		this.nonce = 0;
 	}
 
 	calculateHash() {
-		return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)).toString();
+		return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data) + this.nonce).toString();
+	}
+
+	// In order to protect our blockchain, we need to be able to ensure that we have a valid block on our hands
+	// One way to do this is via Proof of Work, or method that makes a machine perform some certain amount of work
+	// to prove that it's a valid machine and that the block is valid. The more a machine has to work, the more valid
+	// the block is, basically.
+	mineBlock(difficulty = 5) {
+		while(this.hash.substring(0,difficulty) !== Array(difficulty+1).join('0')) {
+			this.nonce++
+			this.hash = this.calculateHash();
+		}
+		console.log("Block mined: " + this.hash)
 	}
 }
 
 class Blockchain {
 	constructor() {
 		this.chain = [this.createGenesisBlock()];
+		this.difficulty = 6;
 	}
 
 	createGenesisBlock() {
@@ -29,8 +47,14 @@ class Blockchain {
 
 	addBlock(newBlock) {
 		newBlock.previousHash = this.getLatestBlock().hash;
-		newBlock.hash = newBlock.calculateHash();
-		// Usually need more checks to add a block, but this is enough for our purposes
+		
+		// In our original system, we just added the blocks without any kind of verification.
+		// We've just added a Proof of Work via "mineBlock", which will add the block only under a certain condition is met
+		// For now, we can implement it like so:
+		newBlock.mineBlock(this.difficulty);
+		// We don't have to create a new hash for "newBlock" like we did in our original version 
+		// because the "mineBlock" already creates the hash for us anyways.
+
 		this.chain.push(newBlock);
 	}
 
@@ -54,15 +78,28 @@ class Blockchain {
 }
 
 let myCoin = new Blockchain();
+
+console.log('Mining Block 1...')
 myCoin.addBlock(new Block(1, "05/29/2019", { amount: 4}));
+
+console.log('Mining Block 2...');
 myCoin.addBlock(new Block(2, "05/30/2019", { amount: 10}));
+
+
+
+/*
+// The code below was used prior to the addition of the 'mineBlock' function inside the Block class.
+// In other words, the code below was just to check if the blockchain could validate its blocks properly.
 
 console.log("Is blockchain valid #1?\n"+myCoin.isChainValid());
 
 myCoin.chain[1].data = { amount: 100 };
 console.log("Is blockchain valid #2?\n"+myCoin.isChainValid());
+// Because the block's recorded has his not the same as the recalculated hash...
 
 myCoin.chain[1].hash = myCoin.chain[1].calculateHash();
 console.log("Is blockchain valid #3?\n"+myCoin.isChainValid());
+// Because the next block's recorded hash of the current block is not the same as the current block's recorded hash.
 
 //console.log(JSON.stringify(myCoin, null, 4));
+*/
